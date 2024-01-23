1460747b20e38b21c5d25100bce7430757cf5745
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-01-23 23:40:26 +0530

    Update PostController.js

--- server/controller/PostController.js
+++ server/controller/PostController.js
@@ -8,8 +8,14 @@ module.exports.addpost = async (req,res)=>{
     // const {post_author_img} = req.file.path;
     console.log('this=',req.body);
     try{
-        const pdata = new Post({post_author,post_category,post_title,post_para});
-        await pdata.save();
+        const data = new Post({post_author,post_category,post_title,post_para});
+        const result = await data.save();
+        console.log(result);
+        if (result) {
+            res.status(201).json({ message: 'Post saved' });
+        } else {
+            res.status(500).json({ message: 'Post not saved' })
+        }
         
     }catch(e){
         console.log(e);
