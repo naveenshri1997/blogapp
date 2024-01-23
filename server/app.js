55465fef13947896614bbad69e552fe6d17ecf5f
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-01-23 22:52:47 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,30 +7,34 @@ const bodyParser = require('body-parser')
 const app = express();
 app.use(express.json());
 dotenv.config({path:'./config.env'});
+
 require('./db/conn');
-app.use(require('./router/auth'));
 
 app.use(cors());
-// app.use(
-//     cors({
-//       origin: "https://blogclient-f7iq.onrender.com/",
-//     })
-// );
+app.use(
+    cors({
+      origin: "https://blogclient-f7iq.onrender.com/",
+    })
+);
+
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(
-//     cors({
-//         origin: "*",
-//     })
-// )
+app.use(
+    cors({
+        origin: "*",
+    })
+)
+
+
+app.use(express.json());
+app.use(require('./router/auth'));
+const PORT= process.env.PORT||5000||'localhost';
 
 app.get('/', (req, res) => {
     res.send('hello from server');
 });
-
-const PORT= process.env.PORT||5000||'localhost';
-
-
 app.listen(PORT,(req,res)=>{
     console.log('listen');
 })
