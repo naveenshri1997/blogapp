1138b0093e41628a7f7acff70e82b81303ce0f63
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-01-24 11:24:20 +0530

    Update auth.js

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,9 +1,25 @@
 const express= require('express');
 const router = express.Router();
+const multer = require('multer');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        console.log(req.category);
+        return cb(null, "./public/upload");
+    },
+    filename: function (req, file, cb) {
+
+        return cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+const upload = multer({ storage });
+const uploadPhoto = require("../middleware/uploads");
+
 const User = require('../model/userSchema');
 const Post = require('../model/postSchema');
 const {addpost}= require('../controller/PostController');
-router.post('/addpost',addpost);
+router.post('/addpost', uploadPhoto.single('image'),addpost);
 router.post('/login', async(req,res)=>{    
     try {
         const {username,password} = req.body;
@@ -11,7 +27,7 @@ router.post('/login', async(req,res)=>{
             res.status(400).json({'msg':'username or password not entered!'})
         }        
         const userLogin = await User.findOne({username:username})
-         res.setHeader('Access-Control-Allow-Credentials', 'true');   
+
         if(!password == userLogin.password){
             res.status(400).json({'msg':'invalid password'});
         }else{
