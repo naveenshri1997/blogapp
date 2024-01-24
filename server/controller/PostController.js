626d9d447ee53a339b656507ac57d97157c6a91f
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-01-24 11:25:52 +0530

    Update PostController.js

--- server/controller/PostController.js
+++ server/controller/PostController.js
@@ -4,17 +4,18 @@ const Post = require('../model/postSchema');
 const fs = require('fs');
 
 module.exports.addpost = async (req,res)=>{
-    const {post_author,post_category,post_title,post_para}= req.body;
-    // const {post_author_img} = req.file.path;
-    console.log('this=',req.body);
+    const {post_author,category,post_title,post_para}= req.body;
+    const image = req.file.path;
+    console.log(image);
+    console.log(req.file, req.body);
     try{
-        const data = new Post({post_author,post_category,post_title,post_para});
+        const data = new Post({post_author,category,post_title,post_para,image});
         const result = await data.save();
         console.log(result);
         if (result) {
-            res.status(201).json({ message: 'Post saved' });
+            res.status(201).json({ message: 'post saved' });
         } else {
-            res.status(500).json({ message: 'Post not saved' })
+            res.status(500).json({ message: 'post not saved' })
         }
         
     }catch(e){
